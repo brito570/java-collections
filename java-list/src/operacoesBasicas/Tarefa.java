33a4cf4013544ac0fb8130c11662840ab28e38f8
brito570 <lucas.alves.lima.brito@gmail.com>
2024-05-17 12:45:07 -0300

    Update listaTarefa
    Signed-off-by: brito570 <lucas.alves.lima.brito@gmail.com>

---
+++ java-list/src/operacoesBasicas/Tarefa.java
@@ -0,0 +1,31 @@
+package operacoesBasicas;
+
+public class Tarefa {
+	
+	// atributo
+	
+	private String descricao;
+	
+	
+	public Tarefa(String descricao) {
+		
+		this.descricao = descricao;
+		
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	@Override
+	public String toString() {
+		return descricao;
+	}
+	
+	
+
+	
+	
+	
+	
+}
